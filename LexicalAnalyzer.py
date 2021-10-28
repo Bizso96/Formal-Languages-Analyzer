c43cb7ae9ae926817ed974ab6d3c98c76d92ccbf
Bizso96 <bizso96@gmail.com>
2021-10-29 00:07:59 +0300

    Finished declarations, assignments, expressions lexical analysis (except for list and dictionary)

--- LexicalAnalyzer.py
+++ LexicalAnalyzer.py
@@ -1,15 +1,50 @@
-from CustomHashMap import CustomHashMap
+import re
 
+from CustomHashMap import CustomHashMap
+from LexicalError import LexicalError
+
+PIFOutputFileName = "PIF.out"
+STOutputFileName = "ST.out"
+CTOutputFileName = "CT.out"
 
 class LexicalAnalyzer:
     def __init__(self):
         self.file = None
-        self.symbolTable = CustomHashMap(64)
+        self.symbolTable = CustomHashMap(16)
+        self.constantTable = CustomHashMap(16)
+        self.programInternalForm = []
+
         self.fileContent = []
         self.reservedWords = []
+        self.typeNames = []
         self.separators = []
-        self.operators = []
-        self.tokenList = []
+        self.arithmetic_operators = []
+        self.logical_operators = []
+        self.relational_operators = []
+        self.assignment_operators = []
+        self.tokens = []
+
+        self.functionMapping = {
+            "enter": self.checkProgram,
+            "integer": self.checkDeclaration,
+            "character": self.checkDeclaration,
+            "string": self.checkDeclaration,
+            "boolean": self.checkDeclaration,
+            "list": self.notImplemented,
+            "dictionary": self.notImplemented,
+
+        }
+
+        self.typeMapping = {
+            "integer": self.checkIntegerConstant,
+            "character": self.checkCharacterConstant,
+            "string": self.checkStringConstant,
+            "boolean": self.checkBooleanConstant,
+            "list": self.notImplemented,
+            "dictionary": self.notImplemented
+        }
+
+        self.tokenIndex = 0
 
     def open_file(self, filename):
         self.file = open("input/" + filename, "r")
@@ -24,6 +59,46 @@ class LexicalAnalyzer:
         if not self.file.closed:
             self.file.close()
 
+    def writeReport(self):
+        PIFfile = open("output/" + PIFOutputFileName, "w+")
+        STFile = open("output/" + STOutputFileName, "w+")
+        CTFile = open("output/" + CTOutputFileName, "w+")
+        if PIFfile.closed or STFile.closed or CTFile.closed:
+            return False
+
+        PIFfile.write("Token | ST/CT position\n")
+
+        for i in self.programInternalForm:
+            PIFfile.write(str(i) + "\n")
+
+        PIFfile.close()
+
+        STFile.write("Position | Tokens\n")
+        for i in range(self.symbolTable.capacity):
+            st_list = self.symbolTable.get(i)
+            if st_list is None:
+                STFile.write(str(i) + " None\n")
+            else:
+                STFile.write(str(i) + " - " + str(st_list) + "\n")
+
+        STFile.close()
+
+        CTFile.write("Position | Tokens\n")
+        for i in range(self.constantTable.capacity):
+            st_list = self.constantTable.get(i)
+            if st_list is None:
+                CTFile.write(str(i) + " None\n")
+            else:
+                CTFile.write(str(i) + " - " + str(st_list) + "\n")
+
+        CTFile.close()
+
+
+
+
+
+        return True
+
     def get_file_content(self):
         return self.fileContent
 
@@ -32,14 +107,240 @@ class LexicalAnalyzer:
         if self.file.closed:
             return False
 
-        self.reservedWords = token_file.readline().split(":")[1].strip().split(",")
-        self.separators = token_file.readline().split(":")[1].replace('"', '').strip().split(",")
-        self.separators.append(" ")
-        self.operators = token_file.readline().split(":")[1].replace('"', '').strip().split(",")
-
-        print(self.reservedWords)
-        print(self.separators)
-        print(self.operators)
+        self.reservedWords = token_file.readline().split(":")[1].replace(' ', '').strip().split(",")
+        self.typeNames = token_file.readline().split(":")[1].replace(' ', '').strip().split(",")
+        self.separators = token_file.readline().split(":")[1].replace('"', '').replace(' ', '').strip().split(",")
+        self.arithmetic_operators = token_file.readline().split(":")[1].replace('"', '').replace(' ', '').strip().split(",")
+        self.logical_operators = token_file.readline().split(":")[1].replace('"', '').replace(' ', '').strip().split(",")
+        self.relational_operators = token_file.readline().split(":")[1].replace('"', '').replace(' ', '').strip().split(",")
+        self.assignment_operators = token_file.readline().split(":")[1].replace('"', '').replace(' ', '').strip().split(",")
 
     def tokenize(self):
-        pass
+        print('(' + '\".*\" |' + '[' + r'\s' + '\\'.join(self.separators) + '])')
+        rePattern = re.compile('(' + '\".*\"|' + '[' + r'\s' + '\\'.join(self.separators) + '])')
+        l = 0
+        for line in self.fileContent:
+            l += 1
+            initialSplit = rePattern.split(line)
+            for i in initialSplit:
+                if i and not i.isspace():
+                    self.tokens.append(Token(l, i))
+
+    def addToSymbolTable(self, value):
+        if not self.symbolTable.search(value):
+            self.symbolTable.add(value)
+
+    def addToConstantTable(self, value):
+        if not self.constantTable.search(value):
+            self.constantTable.add(value)
+
+    def analyze(self):
+        self.tokenIndex = 0
+        try:
+            self.analyzeCurrent()
+            print("Lexically correct")
+            self.writeReport()
+        except LexicalError as lexicalError:
+            print("Lexical error on line " + str(lexicalError.line) + ": " + lexicalError.message)
+            self.writeReport()
+
+    def analyzeCurrent(self):
+        if self.tokenIndex >= len(self.tokens):
+            return
+
+        if self.currentToken() not in self.reservedWords:
+            if self.symbolTable.search(self.currentToken()) is None:
+                raise LexicalError(self.currentLine(), "Unknown token - " + self.currentToken())
+            if self.tokenIndex + 1 < len(self.tokens) and self.tokens[self.tokenIndex + 1].value in self.assignment_operators:
+                self.programInternalForm.append(PIFPair(self.currentToken(), self.symbolTable.search(self.currentToken())))
+                self.tokenIndex += 1
+                self.checkAssignment()
+            else:
+                self.checkExpression()
+        else:
+            self.functionMapping[self.currentToken()]()
+
+    def currentToken(self):
+        return self.tokens[self.tokenIndex].value
+
+    def nextToken(self):
+        if self.tokenIndex < len(self.tokens) - 1:
+            return self.tokens[self.tokenIndex + 1].value
+
+        return None
+
+    def previousToken(self):
+        if self.tokenIndex > 1:
+            return self.tokens[self.tokenIndex - 1].value
+
+        return None
+
+    def currentLine(self):
+        return self.tokens[self.tokenIndex].line
+
+    def endOfProgram(self):
+        if self.tokenIndex >= len(self.tokens):
+            return True
+        return False
+
+    # region Language Elements
+
+    def checkVariable(self, value):
+        return value not in self.reservedWords and re.fullmatch(r'[a-zA-Z][a-zA-Z0-9]*', value)
+
+    def checkTypeName(self, value):
+        return value in self.typeNames
+
+    def checkIntegerConstant(self, value):
+        return re.fullmatch(r'[-+]?[1-9][0-9]*', value) is not None
+
+    def checkBooleanConstant(self, value):
+        return value in ["true", "false"]
+
+    def checkCharacterConstant(self, value):
+        return re.fullmatch(r"\'[0-9a-zA-Z!?$@&.]\'", value) is not None
+
+    def checkStringConstant(self, value):
+        return re.fullmatch(r'\"[a-zA-Z0-9!?$@&.\s]*\"', value) is not None
+
+    def checkListConstant(self, value):
+        return False;
+
+    def checkDictionaryConstant(self, value):
+        return False
+
+    def checkConstant(self, value):
+        return self.checkIntegerConstant(value) or self.checkBooleanConstant(value) or self.checkCharacterConstant(value) or self.checkStringConstant(value)
+
+    # Enter keyword
+    def checkProgram(self):
+        if self.currentToken() != "enter":
+            raise LexicalError(self.currentLine(), "Program expecting enter keyword")
+        self.programInternalForm.append(PIFPair(self.currentToken(), None))
+
+        self.tokenIndex += 1
+        self.checkActionList()
+
+        if self.currentToken() != "exit":
+            raise LexicalError(self.currentLine(), "Program expecting exit keyword")
+        self.programInternalForm.append(PIFPair(self.currentToken(), None))
+
+    # Return on finding closing token
+    def checkActionList(self):
+        while self.tokenIndex < len(self.tokens):
+            if self.currentToken() == "exit" or self.currentToken() == "}":
+                return
+            self.checkAction()
+            self.tokenIndex += 1
+
+        raise LexicalError(self.tokens[self.tokenIndex - 1].line, "Missing action list closing token")
+
+    def checkAction(self):
+        self.analyzeCurrent()
+        if self.currentToken() != ";":
+            raise LexicalError(self.currentLine(), "Missing semicolon")
+
+        self.programInternalForm.append(PIFPair(self.currentToken(), None))
+
+    def checkDeclaration(self):
+        if self.currentToken() not in self.typeNames:
+            raise LexicalError(self.currentLine(), "Not a valid type name")
+
+        self.programInternalForm.append(PIFPair(self.currentToken(), None))
+
+        self.tokenIndex += 1
+        if self.endOfProgram():
+            raise LexicalError(self.currentLine() - 1, "Missing variable declaration")
+
+        if not self.checkVariable(self.currentToken()):
+            raise LexicalError(self.currentLine(), "Wrong variable format")
+
+        if self.symbolTable.search(self.currentToken()) is None:
+            st_hash, list_position = self.symbolTable.add(self.currentToken())
+            self.programInternalForm.append(PIFPair(self.currentToken(), (st_hash, list_position)))
+        else:
+            raise LexicalError(self.currentLine(), "Variable already declared")
+
+        self.tokenIndex += 1
+
+    def checkAssignment(self):
+        if self.currentToken() not in self.assignment_operators:
+            raise LexicalError(self.currentLine(), "Expected assignment operator")
+
+        self.programInternalForm.append(PIFPair(self.currentToken(), None))
+
+        self.tokenIndex += 1
+        self.checkExpression()
+
+    def checkExpression(self):
+        if self.checkConstant(self.currentToken()) or self.symbolTable.search(self.currentToken()) is not None:
+            if self.checkConstant(self.currentToken()):
+                position = self.constantTable.search(self.currentToken())
+
+                if position is None:
+                    ct_hash, list_position = self.constantTable.add(self.currentToken())
+                else:
+                    ct_hash, list_position = position
+
+                self.programInternalForm.append(PIFPair(self.currentToken(), (ct_hash, list_position)))
+            else:
+                self.programInternalForm.append(PIFPair(self.currentToken(), self.symbolTable.search(self.currentToken())))
+                if self.nextToken() == "[":
+                    self.tokenIndex += 1
+                    self.programInternalForm.append(PIFPair(self.currentToken(), None))
+                    self.tokenIndex += 1
+                    self.checkExpression()
+                    if self.currentToken() == "]":
+                        self.programInternalForm.append(PIFPair(self.currentToken(), None))
+                    else:
+                        raise LexicalError(self.currentLine(), "Designation not closed")
+
+            self.tokenIndex += 1
+
+            if self.currentToken() in self.arithmetic_operators:
+                self.programInternalForm.append(PIFPair(self.currentToken(), None))
+                self.tokenIndex += 1
+                self.checkExpression()
+
+        else:
+            raise LexicalError(self.currentLine(), "Unexpected element in expression")
+
+
+
+
+    def notImplemented(self):
+        print("Not implemented")
+
+    # endregion
+
+class Token:
+    def __init__(self, line, value):
+        self._line = line
+        self._value = value
+
+    @property
+    def line(self):
+        return self._line
+
+    @property
+    def value(self):
+        return self._value
+
+    def __str__(self):
+        return str(self._line) + " " + self._value
+
+class PIFPair:
+    def __init__(self, token, tablePosition):
+        self._token = token
+        self._tablePosition = tablePosition
+
+    @property
+    def token(self):
+        return self._token
+
+    @property
+    def tablePosition(self):
+        return self._tablePosition
+
+    def __str__(self):
+        return str(self._token) + " - " + str(self._tablePosition)
--- main.py
+++ main.py
@@ -5,7 +5,17 @@ if __name__ == '__main__':
     lexical_analyzer = LexicalAnalyzer()
     lexical_analyzer.open_file("p1.txt")
     lexical_analyzer.read_tokens_input("token.in")
-    for line in lexical_analyzer.get_file_content():
-        print(line, end="")
+
+    print(lexical_analyzer.reservedWords)
+    print(lexical_analyzer.typeNames)
+    print(lexical_analyzer.separators)
+    print(lexical_analyzer.arithmetic_operators)
+    print(lexical_analyzer.logical_operators)
+    print(lexical_analyzer.relational_operators)
+    print(lexical_analyzer.assignment_operators)
+
     lexical_analyzer.close_file()
 
+    lexical_analyzer.tokenize()
+
+    lexical_analyzer.analyze()
